a87d259f32055dcd4fed9bd4504aae73fb7dbf66
SergioBalca <3896@holbertonschool.com>
2022-03-23 10:55:54 -0700

    function that inserts a node as the left-child of another node

--- binary_trees.h
+++ binary_trees.h
@@ -4,6 +4,7 @@
 /*Libraries*/
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -24,6 +25,6 @@ typedef struct binary_tree_s
 /*Prototypes*/
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-binary_tree_t *create_node(int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 #endif /* _BINARY_TREES_H_ */
